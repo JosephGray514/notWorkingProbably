6413327cd9075710067307f695da402dcb9890d3
JosephGray514 <josephgray514@gmail.com>
2024-01-05 15:54:54 -0600

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req,res) =>{
+    res.status(200).send('Hola')
+});
+
+// Iniciar el servidor en un puerto específico
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
