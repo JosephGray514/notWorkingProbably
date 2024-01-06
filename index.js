af7cfe5cc564b83ecb1d9388ccd3b7ac0181364a
JosephGray514 <josephgray514@gmail.com>
2024-01-05 19:59:30 -0600

    Webhooks

--- index.js
+++ index.js
@@ -2,6 +2,33 @@ import express from 'express'
 
 const app = express()
 
+// Ruta para el método GET
+app.get('/webhook', (req, res) => {
+    console.log('GET: webhook');
+
+    const VERIFY_TOKEN = 'stringUnicoParaTuAplicacion'
+
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+
+    if(mode && token){
+        if(mode === 'subscribe' && token === VERIFY_TOKEN){
+            console.log('WEBHOOK VERIFICADO')
+            res.status(200).send(challenge)
+        }else{
+            res.sendStatus(404);
+        }
+    }else{
+        res.sendStatus(404);
+    }
+});
+
+// Ruta para el método POST
+app.post('/webhook', (req, res) => {
+    
+});
+
 app.get('/', (req,res) =>{
     res.status(200).send('Hello World I am Joseph')
 });
