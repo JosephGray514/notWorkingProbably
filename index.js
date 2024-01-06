45dcc114a8b238e27e36babf9638450b2c0b03d3
JosephGray514 <josephgray514@gmail.com>
2024-01-05 20:20:39 -0600

    llm

--- index.js
+++ index.js
@@ -1,36 +1,81 @@
-import express from 'express'
+import express from "express";
+import { config } from "dotenv";
+config();
 
-const app = express()
+import { TextLoader } from "langchain/document_loaders/fs/text";
+import { CharacterTextSplitter } from "langchain/text_splitter";
+
+///////////
+
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { FaissStore } from "langchain/vectorstores/faiss";
+import { OpenAI } from "langchain/llms/openai";
+import { RetrievalQAChain, loadQAStuffChain } from "langchain/chains";
+
+const app = express();
 
 // Ruta para el método GET
-app.get('/webhook', (req, res) => {
-    console.log('GET: webhook');
+app.get("/webhook", (req, res) => {
+  console.log("GET: webhook");
 
-    const VERIFY_TOKEN = 'stringUnicoParaTuAplicacion'
+  const VERIFY_TOKEN = "stringUnicoParaTuAplicacion";
 
-    const mode = req.query['hub.mode'];
-    const token = req.query['hub.verify_token'];
-    const challenge = req.query['hub.challenge'];
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
 
-    if(mode && token){
-        if(mode === 'subscribe' && token === VERIFY_TOKEN){
-            console.log('WEBHOOK VERIFICADO')
-            res.status(200).send(challenge)
-        }else{
-            res.sendStatus(404);
-        }
-    }else{
-        res.sendStatus(404);
+  if (mode && token) {
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+      console.log("WEBHOOK VERIFICADO");
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(404);
     }
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 // Ruta para el método POST
-app.post('/webhook', (req, res) => {
-    
+app.post("/webhook", (req, res) => {
+
 });
 
-app.get('/', (req,res) =>{
-    res.status(200).send('Hello World I am Joseph')
+app.get("/", async (req, res) => {
+  const loader = new TextLoader("./Texto.txt");
+
+  const docs = await loader.load();
+
+  const splitter = new CharacterTextSplitter({
+    chunkSize: 200,
+    chunkOverlap: 50,
+  });
+
+  const documents = await splitter.splitDocuments(docs);
+  console.log(documents);
+
+  const embeddings = new OpenAIEmbeddings();
+
+  const vectorstore = await FaissStore.fromDocuments(documents, embeddings);
+  await vectorstore.save("./");
+
+  ////////////////
+
+  const vectorStore = await FaissStore.load("./", embeddings);
+
+  const model = new OpenAI({ temperature: 0 });
+
+  const chain = new RetrievalQAChain({
+    combineDocumentsChain: loadQAStuffChain(model),
+    retriever: vectorStore.asRetriever(),
+    returnSourceDocuments: true,
+  });
+
+  const response = await chain.call({
+    query: "Como puedo contactarlos?",
+  });
+
+  res.status(200).send(response.text);
 });
 
 // Iniciar el servidor en un puerto específico
